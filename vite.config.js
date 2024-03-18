77ec98d84be13f6ed15a6065145e251093ce6816
Acksout <157364563+Acksout@users.noreply.github.com>
2024-03-18 15:13:03 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: 'https://krazy-talk.up.railway.app',
+    // base: 'https://krazy-talk.up.railway.app',
 })
